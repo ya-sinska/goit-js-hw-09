a270a06f7ddd0ae8d075a3a10752df65f1ff3540
Romanna Yasinska <r.bren.1991@gmail.com>
2021-12-23 21:21:56 +0200

    Update tast-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    body: document.querySelector("body"),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+let intervalID;
 
+refs.startBtn.addEventListener('click', onStartClick)
+refs.stopBtn.addEventListener('click', onStopClick)
+
+function onStartClick(e) {
+    console.log('click start')
+    refs.startBtn.disabled = 'disabled';
+    refs.stopBtn.disabled = '';
+    intervalID = setInterval (changeBGC, 1000)
+}
+function changeBGC() {
+ refs.body.style.backgroundColor = `${getRandomHexColor()}`
+}
+
+function onStopClick(e) {
+    console.log('click stop')
+    refs.startBtn.disabled = '';
+    refs.stopBtn.disabled = 'disabled';
+    clearInterval(intervalID )
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

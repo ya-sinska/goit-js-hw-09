97c0f66faef3a2e83e558a1328296e3404bf5402
Romanna Yasinska <r.bren.1991@gmail.com>
2021-12-25 03:16:21 +0200

    Update timer task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,8 +2,15 @@ import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
-    btnStart: document.querySelector('button')
+    btnStart: document.querySelector('button'),
+    inputCalendar: document.querySelector('input'),
+    days: document.querySelector('span[data-days]'),
+    hours: document.querySelector('span[data-hours]'),
+    minutes: document.querySelector('span[data-minutes]'),
+    seconds: document.querySelector('span[data-seconds]'),
 }
+
+let start;
 // Об єкт для властивостей бібліотеки flatpickr (пікер з календаря)
 const options = {
   enableTime: true,
@@ -14,9 +21,11 @@ const options = {
         if (selectedDates[0]>options.defaultDate) {
             console.log('робимо активним старт');
             refs.btnStart.disabled = '';
+            start = selectedDates[0].getTime();
+            // Слухач для кнопки
+            refs.btnStart.addEventListener('click', onBtnStartClick)
         } else {
             alert("Please choose a date in the future")
-            refs.btnStart.disabled = 'disabled';
             console.log('старт відхилено не вірно вказана дата');
         }
   },
@@ -25,43 +34,124 @@ const options = {
 // ініціалізація flatpickr в проекті
 flatpickr("#datetime-picker", options);
 
-// Таймер
-const timer = {
-    start() {
-        const startTime = Date.now()
-    
-    setInterval(() => {
-        const currentTime = Date.now()
-        const deltaTime = currentTime-startTime
-        const { days, hours, minutes, seconds } = convertMs(deltaTime)
-        console.log(`${days}:${hours}:${minutes}:${seconds }`)
-    }, 1000)
+// При кліку по кнопці запускаємо таймер
+function onBtnStartClick() {
+    timer.start(start)
+    // роблю не активним старт після запуску таймера
+    refs.btnStart.disabled = 'disabled';
+}
+// Створила клас таймер модель підрахунку часу
+class Timer {
+    constructor({onTick}) {
+        this.isActive = false;
+        this.intervalID = null;
+        this.onTick = onTick;
     }
-}
+    // функція старту таймера
+    start(startTime) {
+        if (this.isActive) {
+            return;
+        }
+        this.isActive = true;
 
-// timer.start()
-
-// функція яка форматує дату до формату хх:хх:хх
-function addLeadingZero(value) {
+        this.intervalID = setInterval(() => {
+        const currentTime = Date.now()
+        const deltaTime = startTime - currentTime
+        const timerTime = this.convertMs(deltaTime)
+        console.log(timerTime)
+        if (deltaTime<1000) {
+            clearInterval(this.intervalID);
+            this.isActive = false;
+        } 
+        this.onTick(timerTime)    
+        }, 1000)
+    }
+    // функція яка форматує дату до формату хх:хх:хх
+    addLeadingZero(value) {
     return String(value).padStart(2, '0');
-}
+    }
 
 // функція для підрахунку часу з юнікс
-function convertMs(ms) {
+    convertMs(ms) {
   // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60; 
-  const hour = minute * 60;
-  const day = hour * 24;
+    const second = 1000;
+    const minute = second * 60; 
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  // Remaining days
-  const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-  return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds };
+    }
 }
+// Створила екземпляр таймера
+const timer = new Timer({
+    onTick: updateClock
+});
+
+// функція для роботи з візуалом
+function updateClock({ days, hours, minutes, seconds }) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.minutes.textContent = `${minutes}`;
+    refs.seconds.textContent = `${seconds}`;
+   
+}
+// ======== до створення класу таймера ========
+// Таймер для відрахунку часу назад 
+// const timer = {
+//     isActive: false,
+//     intervalID: null,
+
+//     start(startTime) {
+//         if (this.isActive) {
+//             return;
+//         }
+//         this.isActive = true;
+
+//         this.intervalID = setInterval(() => {
+//         const currentTime = Date.now()
+//         const deltaTime = startTime - currentTime
+//         const timerTime = convertMs(deltaTime)
+//         console.log(timerTime)
+//         if (deltaTime<1000) {
+//             clearInterval(this.intervalID);
+//             this.isActive = false;
+//         } 
+//         updateClock(timerTime)    
+//         }, 1000)
+//     }
+// }
+
+// функція яка форматує дату до формату хх:хх:хх
+// function addLeadingZero(value) {
+//     return String(value).padStart(2, '0');
+// }
+
+// функція для підрахунку часу з юнікс
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60; 
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   // Remaining hours
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   // Remaining minutes
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   // Remaining seconds
+//   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+//   return { days, hours, minutes, seconds };
+// }
+
